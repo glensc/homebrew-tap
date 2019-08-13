bf77a3406c420b037fb35f7de7cc6089b14540c4
Elan Ruusamäe <glen@delfi.ee>
2019-08-13 19:17:06 +0300

    cleanup: use configure_arg

--- Formula/php@7.1-memcache.rb
+++ Formula/php@7.1-memcache.rb
@@ -6,10 +6,5 @@ class PhpAT71Memcache < PhpExtensionFormula
   url "https://github.com/websupport-sk/pecl-memcache/archive/e702b5f91/memcache-3.0.9-e702b5f91.tar.gz"
   sha256 "4a2b13e80074236054ee88c8461578a780d96698c86274303cb8b4b52037b759"
 
-  def install
-    system php_parent.bin/"phpize"
-    system "./configure", *configure_args, "--with-zlib-dir=#{Formula["zlib"].opt_prefix}"
-    system "make"
-    (lib/module_path).install "modules/#{extension}.so"
-  end
+  configure_arg "--with-zlib-dir=#{Formula["zlib"].opt_prefix}"
 end
