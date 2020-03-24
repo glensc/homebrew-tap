4cc1b71e98533ba4c4fa43f025c96fb80c3a6b27
Elan Ruusamäe <glen@delfi.ee>
2020-03-24 22:54:27 +0200

    Add php@7.1-imagick
    Extracted from kabel/pecl/php@7.1-imagick before 7.1 removal:
    - https://github.com/kabel/homebrew-pecl/tree/10839ac

---
+++ Formula/php@7.1-imagick.rb
@@ -0,0 +1,11 @@
+require_relative "../lib/php_extension_formula"
+
+class PhpAT71Imagick < PhpExtensionFormula
+  extension_dsl "Create and modify images using the ImageMagick API"
+
+  url "https://pecl.php.net/get/imagick-3.4.4.tgz"
+  sha256 "8dd5aa16465c218651fc8993e1faecd982e6a597870fd4b937e9ece02d567077"
+  revision 2
+
+  depends_on "imagemagick"
+end
