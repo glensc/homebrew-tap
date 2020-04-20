4863c95158a2ee19be3fe7c0669b91026a97026a
Elan Ruusamäe <glen@delfi.ee>
2020-04-20 10:33:53 +0300

    Update to 0.4.7

--- Formula/php@7.1-spx.rb
+++ Formula/php@7.1-spx.rb
@@ -3,8 +3,8 @@ require_relative "../lib/php_pecl_formula"
 class PhpAT71Spx < PhpPeclFormula
   extension_dsl "SPX PHP extension"
   homepage "https://github.com/NoiseByNorthwest/php-spx"
-  url "https://github.com/NoiseByNorthwest/php-spx/archive/v0.2.2.tar.gz"
-  sha256 "7ad22c84258f63a2130c596ec93269f8a0be7ce42629aed8430a388c0162c7cf"
+  url "https://github.com/NoiseByNorthwest/php-spx/archive/v0.4.7.tar.gz"
+  sha256 "d91f8e37500ecb5137e06c0d48ce267844e1060ebba53f03f6d1c54508eadd43"
   head "https://github.com/NoiseByNorthwest/php-spx.git"
 
   configure_arg "--with-zlib-dir=#{MacOS.sdk_path_if_needed}/usr"
