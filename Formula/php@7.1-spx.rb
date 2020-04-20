6a4113f7750314aa80aae18f2140159c94c43539
Elan Ruusamäe <glen@delfi.ee>
2020-04-20 10:04:00 +0300

    Update to use lib/php_pecl_formula

--- Formula/php@7.1-spx.rb
+++ Formula/php@7.1-spx.rb
@@ -1,20 +1,11 @@
-require File.expand_path("../Abstract/abstract-php-extension", __dir__)
+require_relative "../lib/php_pecl_formula"
 
-class Php71Spx < AbstractPhp71Extension
-  init
-  desc "SPX PHP extension"
+class PhpAT71Spx < PhpPeclFormula
+  extension_dsl "SPX PHP extension"
   homepage "https://github.com/NoiseByNorthwest/php-spx"
   url "https://github.com/NoiseByNorthwest/php-spx/archive/v0.2.2.tar.gz"
   sha256 "7ad22c84258f63a2130c596ec93269f8a0be7ce42629aed8430a388c0162c7cf"
   head "https://github.com/NoiseByNorthwest/php-spx.git"
 
-  def install
-    ENV.universal_binary if build.universal?
-
-    safe_phpize
-    system "./configure", "--prefix=#{prefix}", phpconfig
-    system "make"
-    prefix.install "modules/spx.so"
-    write_config_file if build.with? "config-file"
-  end
+  configure_arg "--with-zlib-dir=#{MacOS.sdk_path_if_needed}/usr"
 end
