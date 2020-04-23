931e7e77d7303a4ba625bf7feb6f222499e68822
Elan Ruusamäe <glen@pld-linux.org>
2020-04-23 19:23:37 +0300

    Add megacmd formula to build official mega.nz megacmd

---
+++ Formula/megacmd.rb
@@ -0,0 +1,11 @@
+class Megacmd < Formula
+  desc "Command Line Interactive and Scriptable Application to access MEGA"
+  homepage "https://mega.nz/"
+  url "https://github.com/meganz/MEGAcmd/archive/1.2.0_MacOS.tar.gz"
+  sha256 "909540033a7900f4f7ef491fc69cfd67a3bb69050708a910ca4a0d22e2b1bfdf"
+  head "https://github.com/meganz/MEGAcmd.git"
+
+  # https://github.com/meganz/MEGAcmd#building-megacmd
+  def install
+  end
+end
