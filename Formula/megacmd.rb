0a38b611ff40809e49d4feb837c8857f1649863a
Elan Ruusamäe <glen@pld-linux.org>
2020-04-23 19:23:40 +0300

    Execute code from ./autogen.sh

--- Formula/megacmd.rb
+++ Formula/megacmd.rb
@@ -5,7 +5,12 @@ class Megacmd < Formula
   sha256 "909540033a7900f4f7ef491fc69cfd67a3bb69050708a910ca4a0d22e2b1bfdf"
   head "https://github.com/meganz/MEGAcmd.git"
 
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "libtool" => :build
+
   # https://github.com/meganz/MEGAcmd#building-megacmd
   def install
+    system "autoreconf", "-fi"
   end
 end
