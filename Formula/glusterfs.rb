4baf9e3970a439f99187f8daa0265e0d31fa8e5e
Elan Ruusamäe <glen@pld-linux.org>
2020-10-07 14:57:03 +0300

    Update to 8.2

--- Formula/glusterfs.rb
+++ Formula/glusterfs.rb
@@ -1,25 +1,29 @@
-require 'formula'
-
 class Glusterfs < Formula
-  homepage 'http://www.gluster.org'
-  url 'https://github.com/mhubig/glusterfs.git', 
-      :tag => '838224369845e7566bbbac24ffd70d166a3df422',
-      :using => :git
-  version '3.2git'
-
-  head 'https://github.com/mhubig/glusterfs.git',
+  homepage 'https://www.gluster.org'
+  url 'https://github.com/gluster/glusterfs.git',
+      :tag => "v8.2",
+      :revision => "895183d5a2eceabcfdd81daff87ecab1159c32c6"
+  head 'https://github.com/gluster/glusterfs.git',
        :using => :git
 
   depends_on 'pkg-config' => :build
-  depends_on 'fuse4x'
+  depends_on 'automake' => :build
+  depends_on 'autoconf' => :build
+  depends_on 'libtool' => :build
+  depends_on 'openssl' => :build
+  depends_on 'argp-standalone' => :build
 
   def install
     ENV['ACLOCAL_FLAGS'] = "-I#{HOMEBREW_PREFIX}/share/aclocal"
     
-    args = ["--disable-debug", "--disable-dependency-tracking",
-            "--with-launchddir=#{prefix}/Library/LaunchDaemons",
-            "--with-mountutildir=#{prefix}/sbin",
-            "--prefix=#{prefix}"]
+    args = [
+      "--disable-debug",
+      "--disable-dependency-tracking",
+      "--with-launchddir=#{prefix}/Library/LaunchDaemons",
+      "--with-mountutildir=#{prefix}/sbin",
+      "--without-server",
+      "--prefix=#{prefix}"
+    ]
 
     system "./autogen.sh"
     system "./configure", *args
